e2e1fd4ddd39c5399cc7b1ac0130cb7dbc62e9c3
Filipe Brener <filipe@itexto.com.br>
2022-07-07 12:48:40 -0300

    descomentando trem importante

--- php/views/editar.php
+++ php/views/editar.php
@@ -24,7 +24,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="../utils/images/favicon.ico" type="image/ico">
-    <!-- <link rel="stylesheet" href="../../styles/exibir.css"> -->
+    <link rel="stylesheet" href="../../styles/exibir.css">
     <script src="../../scripts/evento.js"></script>
     <script src="../../scripts/nav_bar.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/color-thief/2.3.0/color-thief.umd.js"></script>
